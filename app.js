0037ec89b97166ab53719a9e0d504297873d1047
Ander223 <ander_a_@hotmail.com>
2025-11-03 16:34:27 -0300

    commitando antes do desafio modulo1

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert('Boas vindas ao jogo do número secreto!');
+alert('Boas vindas ao jogo do número secreto!');    // palavra reservada "alert" cria um alerta no navegador
+let numeroSecreto = 5;                                  //palavra reservada "let" para criar variáveis
+let chute = prompt('Escolha um número entre 1 e 10');  // "prompt" mostra uma mensagem na tela e neste caso esta salvando o valor na variavel CHUTE
+
+if ( chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto (5)')  // "console.log" cria uma mensagen no depurador de codigo no navegador
+} 
